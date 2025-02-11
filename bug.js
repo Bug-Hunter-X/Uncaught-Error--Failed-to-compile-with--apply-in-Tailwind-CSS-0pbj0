09ae92de4f812f141d77b8b84855e1b8d3ae2eed
BugHunter-X <askzenai@gmail.com>
2025-02-11 23:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example using Tailwind's @apply directive
+<button class="@apply text-blue-500 hover:text-blue-700">Click Me</button>
+```
+This results in the following error:
+`Uncaught Error: Failed to compile`
